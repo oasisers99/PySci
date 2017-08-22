43838ccefcc03ca751072915c2a8445244f2c44d
oasisers99 <oasisers99@yahoo.com>
2017-08-22 21:33:58 +0930

    init

---
+++ clustering/clustering.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import nltk
+import re
+import os
+import codecs
+from sklearn import feature_extraction
+import mpld3
+
+#reference: http://brandonrose.org/clustering
+#stopwords = nltk.corpus.stopwords.words('english')
+#print(stopwords[:10])
+nltk.download()
