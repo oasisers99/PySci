f17260c995bd96e8e88ffbc957e3f456da9319b0
oasisers99 <oasisers99@yahoo.com>
2017-08-24 23:45:19 +0930

    numpy

--- standford/standford2-numpy.py
+++ standford/standford2-numpy.py
@@ -16,6 +16,23 @@ print(a[0, 1])
 b[0, 0] = 77
 print(a[0, 1]) # print 77
 
+
+a = np.array( [ [1,2,3,4], [5,6,7,8], [9,10,11,12] ] )
+
 row_r1 = a[1, :] #Rank 1 view of the second row of a
 row_r2 = a[1:2, :] #Rank 2 view of the second row of a
-#print(row_r1, row_r1.shape)
+print(row_r1, row_r1.shape) #shape (4,)
+print(row_r2, row_r2.shape)
+
+
+col_r1 = a[:, 1] # prints "[ 2 6 10] (3,)"
+col_r2 = a[:, 1:2] # prints "[[ 2]
+				   # 		  [ 6]
+				   # 		  [10]] (3, 1)
+print(col_r1, col_r1.shape)
+print(col_r2, col_r2.shape)
+
+
+# Integer array indexing
+# When you index numpy arrays using slicing, the resulting array view will always be a subarray of the original array. 
+# In contrast, integer array indexing allows you to construct arbitrary arrays using the data from another array.
